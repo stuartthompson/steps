8fe3f230d8488cf28a8e0635623db22d92c31f08
Stuart Thompson <stuart@stuartthompson.net>
2021-04-23 17:06:54 -0700

    Main: Initial test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+
+pub use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode, KeyEvent},
+    execute, queue, style,
+    terminal::{self, ClearType},
+    Command, Result,
+};
+
+pub fn read_char() -> Result<char> {
+    loop {
+        if let Ok(Event::Key(KeyEvent {
+            code: KeyCode::Char(c),
+            ..
+        })) = event::read()
+        {
+            return Ok(c);
+        }
+    }
 }
+
+fn run<W>(w: &mut W) -> Result<()>
+where
+    W: Write,
+{
+    execute!(w, terminal::EnterAlternateScreen)?;
+
+    terminal::enable_raw_mode()?;
+
+    loop {
+        match read_char()? {
+            '1' =>  {
+                queue!(
+                    w,
+                    cursor::MoveTo(1, 1),
+                    style::SetForegroundColor(style::Color::White),
+                    style::SetBackgroundColor(style::Color::Black),
+                    style::Print("Hello!"),
+                )?
+            },
+            'q' => break,
+            _ => {}
+        };
+
+        w.flush()?;
+    }
+
+    execute!(
+        w,
+        style::ResetColor,
+        cursor::Show,
+        terminal::LeaveAlternateScreen
+    )?;
+
+    terminal::disable_raw_mode()
+}
+
+pub fn buffer_size() -> Result<(u16, u16)> {
+    terminal::size()
+}
+
+fn main() -> Result<()> {
+    let mut stderr = io::stdout();
+    run(&mut stderr)
+}
+
